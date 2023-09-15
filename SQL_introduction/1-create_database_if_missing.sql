17ea0542a196c03ec70f79f960f8fbe3135b6c13
Awash Hedato <awashedato@gmail.com>
2023-09-15 17:56:31 +0300

    1. Create a database update

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,10 @@
---Create a database
-CREATE SCHEMA hbtn_0c_0;
+--First, we need to connect to our MySQL server using the root user and password
+mysql -u root -p
+
+--Next, we can create our database using the following command
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+--If the database already exists, the above command will not fail
+
+--Once the database is created, we can exit the MySQL shell
+exit
