b73cfaa821edaccd747babfb6747aacec719505c
Awash Hedato <awashedato@gmail.com>
2023-09-15 18:18:00 +0300

    4TH UPDATE

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,5 @@
--- Delete database hbtn_0c_0
-DROP DATABASE IF EXISTS hbtn_0c_0;
+/* Delete database hbtn_0c_0 */
+DROP SCHEMA IF EXISTS hbtn_0c_0;
 
---create our database using the following command
-CREATE DATABASE hbtn_0c_0;
+/* create our database using the following command */
+CREATE SCHEMA hbtn_0c_0;
