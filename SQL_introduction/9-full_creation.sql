f94e33b64bdc39c399be3e1a5549a495a307e454
Awash Hedato <awashedato@gmail.com>
2023-09-15 19:41:32 +0300

    Full table creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,3 @@
--- the script creates table second_table
-CREATE TABLE IF NOT EXISTS second_table
-(
-    id int,
-    name varchar(256)
-    score int
-);
-
--- this script inserts new records into a second_table rows
-INSERT INTO second_table(id, name, score)
-VALUES('1', 'John', '10'), ('2', 'Alex', '3'), ('3', 'Bob', '14'), ('4', 'George', '8');
+-- the script creates table second_table and inserts new records into a second_table rows
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
