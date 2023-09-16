7e7dd6d5b9cda785a14faf01aaebcc3957391ac0
Awash Hedato <awashedato@gmail.com>
2023-09-16 07:42:46 +0300

    Unique it update

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- this script creates the table unique_id on MySQL server
-CREATE TABLE IF NOT EXISTS unique_id(
-    id int UNIQUE DEFUALT 1, 
-    name varchar(256)
-    );
+CREATE TABLE IF NOT EXISTS unique_id (
+	id INT UNIQUE DEFAULT 1,
+	name VARCHAR(256)
+	);
